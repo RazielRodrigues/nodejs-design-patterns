c2e52567632ca2e26b32cb7e7252290c43b444b7
Raziel Rodrigues <razielx3@live.com>
2023-01-21 15:21:52 -0300

    personal example

---
+++ PersonalExample/Factories/Ninja/NinjaAreia.js
@@ -0,0 +1,11 @@
+class NinjaAreia {
+    constructor(type = 'Ninja', village = 'Areia') {
+        this.type = type;
+        this.village = village;
+    }
+}
+
+
+module.exports = {
+    NinjaAreia
+}
