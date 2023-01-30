74ac3c180da43e3c0583ab9e28cbbbcf3c30485f
Raziel Rodrigues <razielx3@live.com>
2023-01-29 21:33:57 -0300

    finalizando builder

--- src/PersonalExample/Creational/Builder/index.js
+++ src/PersonalExample/Creational/Builder/index.js
@@ -1,3 +1,19 @@
+/**
+ * O padrão builder é baseado em implementações de criação de objetos
+ * de preferencia usando fluent interface, para isso é usado uma classe
+ * que contém os atributos que vão ser construidos. Em seguida na arvore
+ * hierarquica disso temos então o conceito de ter uma segunda classe
+ * que faz a construção desses atributos através de metodos que geralmente
+ * fazem os sets dessa primeira classe. Em seguida temos a função de build, 
+ * que passa o objeto com as configurações de set da primeira classe.
+ * 
+ * Podemos comparar o padrão build a uma aonde temos a classe principal como
+ * forma e a classe builder como a massa que é injetada nessa forma, essa forma
+ * pode fazer diversos bolos, por exemplo um de chocolate ou de baunilha. aonde
+ * quem insere a massa a builder, mas a forma a abstração da forma como tamanho
+ * e formato estão salvos na classe principal.
+ */
+
 import { ComprasBuilder } from './CarrinhoExample/ComprasBuilder.js';
 import PostBuilder from './PostExample/PostBuilder.js';
 
